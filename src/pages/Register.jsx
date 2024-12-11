597879722805194a15402d927861e92be54f926c
Monir <mrmonir0558@gmail.com>
2024-12-11 11:26:28 +0600

    added lottie reactin to the register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,42 @@
+import Lottie from "lottie-react";
+import registerLottieData from '../assets/lottie/register.json'
+
+
+const Register = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <Lottie className="w-80 lg:w-96" animationData={registerLottieData} />
+                </div>
+                <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl">
+                    <h1 className="text-4xl font-bold text-center mt-8">Register now!</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">
+                                    Forgot password?
+                                </a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,23 @@
-import { createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../layout/MainLayout';
+import Home from '../pages/Home';
+import Register from '../pages/Register';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>Hello world!</div>
+        element: <MainLayout />,
+        errorElement: <h2>Route not found</h2>,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: 'register',
+                element: <Register />
+            }
+        ]
     }
 ]);
 
